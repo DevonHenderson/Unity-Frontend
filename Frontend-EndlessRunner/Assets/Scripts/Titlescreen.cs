b9ea6037a5e0d21d5fdc8f5ab289671cc453be46
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-06-21 22:23:05 +1200

    FIX: Titlescreen now fades out and has transparent text

--- Frontend-EndlessRunner/Assets/Scripts/Titlescreen.cs
+++ Frontend-EndlessRunner/Assets/Scripts/Titlescreen.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Titlescreen : MonoBehaviour
 {
     [SerializeField] private GameObject titlescreen;
+    [SerializeField] private GameObject panel;
     [SerializeField] private GameObject scorePanel;
+    private Image titlescreenImage;
 
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 0f;
+        titlescreenImage = panel.GetComponent<Image>();
     }
-    
+
     public void StartGame()
     {
+        StartCoroutine(FadeOutTitlescreen());
+    }
+
+    private IEnumerator FadeOutTitlescreen()
+    {
+        float duration = 1f;
+        float elapsedTime = 0f;
+        Color initialColor = titlescreenImage.color;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.unscaledDeltaTime;  // Use unscaledDeltaTime because Time.timeScale is 0
+            float alpha = Mathf.Clamp01(1f - (elapsedTime / duration));
+            titlescreenImage.color = new Color(initialColor.r, initialColor.g, initialColor.b, alpha);
+            yield return null;
+        }
+
         titlescreen.SetActive(false);
         scorePanel.SetActive(true);
         Time.timeScale = 1f;
-            
     }
 }
